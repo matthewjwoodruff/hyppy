add3ee7d7df16d37fd33fd8e7db7ab22431e2d9d
Matthew Woodruff <mwoodruff@decisionvis.com>
2014-09-10 09:03:21 +0000

    extra chunks on the side

--- wfg/zn.py
+++ wfg/zn.py
@@ -12,40 +12,43 @@ def zn(rows):
     rows: the set of rows for which to compute hypervolume
     Assume maximization relative to origin.
     """
-    if len(rows) == 0:
-        return 0.0
     nobj = len(rows[0])
     nadir = [float("inf")]*nobj
     zenith = [-float("inf")]*nobj
     vol = 0.0
 
+    # discard points that are at zero
+    rows = [
+            r for r in rows 
+            if not any([x <= 0 for x in r])]
+
+    if len(rows) == 0:
+        return 0.0
+
     for row in rows:
         for i, val in enumerate(row):
             nadir[i] = min([nadir[i], val])
             zenith[i] = max([zenith[i], val])
-    print("nadir {0}".format(nadir))
-    print("zenith {0}".format(zenith))
 
-    add_volume = 1.0
-    for z in zenith:
-        add_volume *= z
-    print("add {0}".format(add_volume))
-    sub_volume = 1.0
-    for z, n in zip(zenith, nadir):
-        sub_volume *= (z-n)
-    print("sub {0}".format(sub_volume))
-    vol += add_volume
-    vol -= sub_volume
+    # add nadir volume
+    nvol = 1.0
+    for n in nadir:
+        nvol *= n
+    vol += nvol
+
+    # add extra chunks on the sides
+    for i, z in enumerate(zenith):
+        point = [n for n in nadir]
+        point[i] = z - nadir[i]
+        chunk = 1.0
+        for x in point:
+            chunk *= x
+        vol += chunk
 
     # translate nadir to origin
     transformed = []
     for row in rows:
         transformed.append([r-n for r, n in zip(row, nadir)])
-
-    # discard extreme points
-    transformed = [
-            r for r in transformed 
-            if not any([x <= 0 for x in r])]
 
     # compute hv on the remaining points
     vol += zn(transformed)
