1daf3a47eb2130f05ab5518a65216c24cc24577b
Matthew Woodruff <mwoodruff@decisionvis.com>
2014-09-11 08:22:23 +0000

    pareto sort on silhouettes

--- wfg/zn.py
+++ wfg/zn.py
@@ -8,6 +8,49 @@ LGPL
 """
 import copy
 
+def sort(rows):
+    """
+    true Pareto sort assuming maximization
+    """
+    archive = []
+    asize = 0
+
+    for row in rows:
+        ai = -1
+        adominate = False # archive dominates
+        sdominate = False # solution dominates
+        nondominate = False # neither dominates
+        while ai < asize - 1:
+            ai += 1
+            adominate = False
+            sdominate = False
+            nondominate = False
+            aobj = archive[ai]
+            for ar, ro in zip(aobj, row):
+                if ar > ro:
+                    adominate = True
+                    if sdominate:
+                        nondominate = True
+                        break # for
+                elif ar < ro:
+                    sdominate = True
+                    if adominate:
+                        nondominate = True
+                        break # for
+            if nondominate:
+                continue
+            if adominate:
+                break
+            if sdominate:
+                archive.pop(ai)
+                ai -= 1
+                asize -= 1
+                continue 
+        if nondominate or sdominate or not adominate:
+            archive.append(row)
+            asize += 1
+    return archive
+
 def zn(rows):
     """
     rows: the set of rows for which to compute hypervolume
@@ -32,35 +75,20 @@ def zn(rows):
     for row in rows:
         for i, val in enumerate(row):
             nadir[i] = min((nadir[i], val))
-            if val > zenith[i]:
-                zenith[i] = val
-                zenith_contributors[i] = copy.copy(row)
-            elif val == zenith[i]:
-                if any(r > zc for r, zc in zip(row, zenith_contributors[i])):
-                    zenith_contributors[i] = copy.copy(row)
 
     # find hypervolume one dimension down for each silhouette
     for axis in range(nobj):
         offset = nadir[axis]
         sil = []
-        for row in zenith_contributors:
+        for row in rows:
             sil.append([row[i] for i in range(nobj) if i != axis])
-        sil = [list(row) for row in list(set([tuple(row) for row in sil]))]
+        sil = sort(sil)
         down_one = zn(sil)
         vol += offset * down_one
-        for row in zenith_contributors:
+        for row in rows:
             row[axis] -= offset
 
-    nadir_hypervolume = 1.0
-    for x in nadir:
-        nadir_hypervolume *= x
-    
-    # translate nadir to origin
-    transformed = []
-    for row in rows:
-        transformed.append([r-n for r, n in zip(row, nadir)])
-
     # compute hv on the translated points
-    vol += zn(transformed)
+    vol += zn(rows)
 
     return vol
